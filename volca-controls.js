732da4ef2af56732accddabc2ad7716b5ebb878e
A'Jahn Kimble <ajahnkimble@pop-os.localdomain>
2022-11-12 02:17:58 -0600

    $branch

--- volca-controls.js
+++ volca-controls.js
@@ -1,4 +1,4 @@
-const drumKeys = ["q", "w", "e", "r", "t", "y"];
+const drumKeys = "qwerty";
 
 const drumButtons = document.getElementsByClassName("drum button surfaces")[0]
   .children;
@@ -10,56 +10,51 @@ const effectButtons = document.getElementsByClassName(
 const chokeButtons = document.getElementsByClassName("choke button surfaces")[0]
   .children;
 
-const triggerKeys = [
-  "a",
-  "s",
-  "d",
-  "f",
-  "g",
-  "h",
-  "j",
-  "k",
-  "z",
-  "x",
-  "c",
-  "v",
-  "b",
-  "n",
-  "m",
-  ",",
-];
+const triggerKeys = "asdfghjkzxcvbnm,";
 
 const triggerButtons = document.getElementsByClassName(
   "trigger button surfaces"
 )[0].children;
 
+const modKeys = "opl;";
+
 document.addEventListener("keydown", ifKeyPressed);
 document.addEventListener("keyup", ifKeyPressed);
 
 let modHeld = false;
+let lastMod = "";
+
 function ifKeyPressed(event) {
-  if (modHeld && event.type === "keydown") {
-    console.log("secondKey", event.key);
+  console.log(event.key);
+  //consider using qwerty for drums, yuiop for choke and 123456 for effects
+  let modEvent = modKeys.indexOf(event.key) >= 0;
+
+  if (!modHeld) {
+    findAndMoveButton(event, triggerKeys, triggerButtons);
+    findAndMoveButton(event, drumKeys, drumButtons);
   }
-  if (!modHeld && event.ctrlKey && event.type === "keydown") {
-    console.log("Mod!");
+
+  if (!modHeld && modEvent) {
     modHeld = true;
-  } else if (modHeld && !event.ctrlKey && event.type === "keyup") {
-    console.log("NO MOD!!");
+    lastMod = event.key;
+  } else if (modHeld && modEvent) {
     modHeld = false;
+  } else if (modHeld) {
+    switch (lastMod) {
+      case "o":
+        findAndMoveButton(event, drumKeys, effectButtons, "ellipse");
+        break;
+      case "l":
+        findAndMoveButton(event, drumKeys, chokeButtons, "ellipse");
+        break;
+    }
   }
-
-  findAndMoveButton(event, triggerKeys, triggerButtons);
-  findAndMoveButton(event, drumKeys, drumButtons);
-
-  //   findAndMoveButton(event, drumKeys, effectButtons, "ellipse");
-  //   findAndMoveButton(event, drumKeys, chokeButtons, "ellipse");
 
   function findAndMoveButton(event, keyArray, buttonGroup, shape = "rect") {
     // if key pressed is in triggerKeys array,
     //    if that key is pressed down, move the rectangle down that correspons to the key
     //    when that key is released, move the rect back to its base position
-    let buttonIndex = keyArray.indexOf(event.key);
+    let buttonIndex = keyArray.indexOf(event.key.toLowerCase());
     if (buttonIndex < 0) return;
     //the triggerButtons array corresponds to the triggerKeys array and the index of these keys is
     //used to find the trigger button
